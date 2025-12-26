3e4211e1da867d6c6f0be746e2eb2d7bd978cf00
rw1030 <rw1030@student.ubc.ca>
2025-12-26 00:13:48 -0800

    finished scanning for parentheses

--- app/main.py
+++ app/main.py
@@ -1,5 +1,77 @@
 import sys
 
+def TokenType():
+    # Single-character tokens.
+    LEFT_PAREN = 'LEFT_PAREN'
+    RIGHT_PAREN = 'RIGHT_PAREN'
+    LEFT_BRACE = 'LEFT_BRACE'
+    RIGHT_BRACE = 'RIGHT_BRACE'
+    COMMA = 'COMMA'
+    DOT = 'DOT'
+    MINUS = 'MINUS'
+    PLUS = 'PLUS'
+    SEMICOLON = 'SEMICOLON'
+    STAR = 'STAR'
+    # one or two character tokens.
+    BANG = 'BANG'
+    BANG_EQUAL = 'BANG_EQUAL'
+    EQUAL = 'EQUAL'
+    EQUAL_EQUAL = 'EQUAL_EQUAL'
+    GREATER = 'GREATER'
+    GREATER_EQUAL = 'GREATER_EQUAL'
+    LESS = 'LESS'
+    LESS_EQUAL = 'LESS_EQUAL'
+    # Literals.
+    IDENTIFIER = 'IDENTIFIER'
+    STRING = 'STRING'
+    NUMBER = 'NUMBER'
+    # Keywords.
+    AND = 'AND'
+    CLASS = 'CLASS'
+    ELSE = 'ELSE'
+    FALSE = 'FALSE'
+    FUN = 'FUN'
+    FOR = 'FOR'
+    IF = 'IF'
+    NIL = 'NIL'
+    OR = 'OR'
+    PRINT = 'PRINT'
+    RETURN = 'RETURN'
+    SUPER = 'SUPER'
+    THIS = 'THIS'
+    TRUE = 'TRUE'
+    VAR = 'VAR'
+    WHILE = 'WHILE'
+    EOF = 'EOF'
+    return locals()
+
+LEFT_PAREN = TokenType()['LEFT_PAREN']
+RIGHT_PAREN = TokenType()['RIGHT_PAREN']
+LEFT_BRACE = TokenType()['LEFT_BRACE']
+RIGHT_BRACE = TokenType()['RIGHT_BRACE']
+COMMA = TokenType()['COMMA']
+DOT = TokenType()['DOT']
+MINUS = TokenType()['MINUS']
+PLUS = TokenType()['PLUS']
+SEMICOLON = TokenType()['SEMICOLON']
+STAR = TokenType()['STAR']
+EOF = TokenType()['EOF']
+
+def scanToken(char):
+    if char=='(': addToken(LEFT_PAREN,char)
+    if char==')': addToken(RIGHT_PAREN,char)
+    if char=='{': addToken(LEFT_BRACE,char)
+    if char=='}': addToken(RIGHT_BRACE,char)
+    if char==',': addToken(COMMA,char)
+    if char=='.': addToken(DOT,char)
+    if char=='-': addToken(MINUS,char)
+    if char=='+': addToken(PLUS,char)
+    if char==';': addToken(SEMICOLON,char)
+    if char=='*': addToken(STAR,char)
+    
+def addToken(tokenType, lexeme=None):
+    text = "null"
+    print(f"{tokenType} {lexeme} {text}")
 
 def main():
     if len(sys.argv) < 3:
@@ -20,9 +92,11 @@ def main():
     print("Logs from your program will appear here!", file=sys.stderr)
 
     if file_contents:
-        raise NotImplementedError("Scanner not implemented")
+        for char in file_contents:
+            scanToken(char)
+        # raise NotImplementedError("Scanner not implemented")
     else:
-        print("EOF  null") # Placeholder, replace this line when implementing the scanner
+        print(f"{EOF} null") # Placeholder, replace this line when implementing the scanner
 
 
 if __name__ == "__main__":
